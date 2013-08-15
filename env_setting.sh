61a33bcedc3ba54b177d3f9cabd4012b2d6848ce
yannik520 <yannik520@gmail.com>
2013-08-15 22:47:20 +0800

    modified makefile

--- env_setting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo $HOME
-TOOLCHAIN_PATH="./tools/arm-elf-toolchain/bin"
-echo $TOOLCHAIN_PATH
-export PATH=$TOOLCHAIN_PATH:$PATH
-export TOPDIR=`pwd`
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ARMEMU_PATH=./tools/armemu
-cd $ARMEMU_PATH
-./build-generic/armemu
-cd -
